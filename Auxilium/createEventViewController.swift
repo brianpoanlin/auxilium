1b8f9a6740a17eab688f4eee14fb6e350582ea9b
Brian Lin <brian.poan.lin@gmail.com>
2017-02-04 23:22:03 -0800

    UI Semi Functional

---
+++ Auxilium/createEventViewController.swift
@@ -0,0 +1,35 @@
+//
+//  createEventViewController.swift
+//  Auxilium
+//
+//  Created by Brian Lin on 2/4/17.
+//  Copyright © 2017 Brian Lin. All rights reserved.
+//
+
+import UIKit
+
+class createEventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
