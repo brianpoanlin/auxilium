0013ed24832dca98b4920f749e1f78d0ca718210
Brian Lin <brian.poan.lin@gmail.com>
2017-02-04 21:33:39 -0800

    Maps

--- Auxilium/Auxilium/EventViewController.swift
+++ Auxilium/Auxilium/EventViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 import Firebase
 import FirebaseDatabase
+import MapKit
 
-class EventViewController: UIViewController {
+
+
+class EventViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var eventName: UILabel!
     @IBOutlet weak var iconImg: UIImageView!
     @IBOutlet weak var dspText: UITextView!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var lat:Double = 40.730872
+    var long:Double = -74.003066
+    var LocationName:String = ""
     
     var toPass:String!
     let eventsRef = FIRDatabase.database().reference(withPath: "master-events/")
@@ -22,7 +30,11 @@ class EventViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("passed \(toPass)")
+        mapView.delegate = self
         self.pullData()
+        
+      
+    
         // Do any additional setup after loading the view.
     }
     
@@ -39,11 +51,28 @@ class EventViewController: UIViewController {
             let imgStr = eventDict.value(forKey: "event_category") as! String
             self.iconImg.image = UIImage(named: "\(imgStr).png")
             self.dspText.text = eventDict.value(forKey: "event_description") as! String?
-            print(eventDict.value(forKey: "event_name"))
+            let corrdinates:NSDictionary = eventDict.object(forKey: "event_location") as! NSDictionary
+            self.lat = corrdinates.value(forKey: "event_Lat") as! Double
+            self.lat = corrdinates.value(forKey: "event_Long") as! Double
+            
+            
+            let dropPin = MKPointAnnotation()
+            dropPin.coordinate = CLLocationCoordinate2DMake(40.730872, -74.003066)
+            dropPin.title = self.LocationName
+            self.mapView.addAnnotation(dropPin)
+            self.mapView.showAnnotations(self.mapView.annotations, animated: true)
+
         })
         
     }
-
+    
+    @IBAction func tappedMaps(_ sender: Any) {
+        print("tapped")
+        let coordinate = CLLocationCoordinate2DMake(lat,long)
+        let mapItem = MKMapItem(placemark: MKPlacemark(coordinate: coordinate, addressDictionary:nil))
+        mapItem.name = LocationName
+        mapItem.openInMaps(launchOptions: [MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving])
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
