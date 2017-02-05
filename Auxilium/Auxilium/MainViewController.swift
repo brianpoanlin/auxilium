f43573b374a8e24ea0ba7810e1acc6d5a1f8393c
Brian Lin <brian.poan.lin@gmail.com>
2017-02-04 18:30:16 -0800

    Working

--- Auxilium/Auxilium/MainViewController.swift
+++ Auxilium/Auxilium/MainViewController.swift
@@ -12,16 +12,29 @@ import FirebaseStorage
 import FirebaseAuth
 import FirebaseDatabase
 
-class MainViewController: UIViewController {
+class eventTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var eventName: UILabel!
+    @IBOutlet weak var eventDescription: UITextView!
+    @IBOutlet weak var eventIcon: UIImageView!
+}
+
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     let ref = FIRDatabase.database().reference()
     let eventsRef = FIRDatabase.database().reference(withPath: "master-events")
 
-    var eventArray : [AnyObject] = []
+    var eventArray : [NSDictionary] = []
+    
+    @IBOutlet weak var tableView: UITableView!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.pullData()
+        tableView.tableFooterView = UIView()
+        tableView.register(UINib(nibName: "eventTableViewCell", bundle: nil), forCellReuseIdentifier: "event")
+
         // Do any additional setup after loading the view.
     }
     
@@ -58,21 +71,51 @@ class MainViewController: UIViewController {
                 
                 
                 for child in snapshot.children.allObjects as? [FIRDataSnapshot] ?? []{
-                    print(child.childSnapshot(forPath: "event_name").value as? String)
-//                    self.eventArray.append(child)
+                    let eventDataSimp: [String: String] =  ["event_name":child.childSnapshot(forPath: "event_name").value as! String,
+                                                             "event_description":child.childSnapshot(forPath: "event_description").value as! String,
+                                                             "event_category":child.childSnapshot(forPath: "event_category").value as! String]
+                    self.eventArray.append(eventDataSimp as NSDictionary)
                 }
-//                
-//
-                
-//                for indObj in self.eventArray as! [NSDictionary] {
-//                    print(indObj.object(forKey: "event_id"))
-//                }
-//                
                 print("DONE")
             })
-        
 
     }
+    
+     func numberOfSectionsInTableView(tableView: UITableView) -> Int{
+        return 1
+    }
+    
+    
+     func tableView(_ tableView:UITableView, numberOfRowsInSection section:Int) -> Int
+    {
+        return self.eventArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "request", for: indexPath) as! eventTableViewCell
+        cell.selectionStyle = .none
+        let currentEvent = eventArray[indexPath.row]
+        cell.eventName.text = currentEvent.value(forKey: "event_name") as? String
+        //        let currentQuestion = questions[indexPath.row]
+        //        cell.questionLabel.text = currentQuestion["text"] as? String
+        //        cell.topicLabel.text = currentQuestion["topic"] as? String
+        //        cell.usernameLabel.text = (currentQuestion["student"] as! PFUser).username!
+        
+                return cell
+    }
+    
+//     func tableView(_ tableView: UITableView!, cellForRowAt indexPath:IndexPath) -> UITableViewCell!
+//    {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "request", for: indexPath) as! eventTableViewCell
+//        cell.selectionStyle = .none
+////        let currentQuestion = questions[indexPath.row]
+////        cell.questionLabel.text = currentQuestion["text"] as? String
+////        cell.topicLabel.text = currentQuestion["topic"] as? String
+////        cell.usernameLabel.text = (currentQuestion["student"] as! PFUser).username!
+//        
+//        return cell
+//
+//    }
     
     
     override func didReceiveMemoryWarning() {
