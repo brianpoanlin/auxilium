4a2c41e4430f03ffa90df5e48802790ad521b784
Brian Lin <brian.poan.lin@gmail.com>
2017-02-05 01:52:26 -0800

    Creating Event

--- Auxilium/createEventViewController.swift
+++ Auxilium/createEventViewController.swift
@@ -11,24 +11,25 @@ import MapKit
 import FirebaseStorage
 import FirebaseAuth
 import FirebaseDatabase
+import CoreLocation
 
 class createEventViewController: UIViewController {
     @IBOutlet weak var eventName: UITextField!
+    @IBOutlet weak var eventCategory: UITextField!
     @IBOutlet weak var eventDescription: UITextView!
     @IBOutlet weak var datePicker: UIDatePicker!
-
     @IBOutlet weak var locationName: UITextField!
     @IBOutlet weak var streetNumber: UITextField!
     @IBOutlet weak var locationState: UITextField!
     @IBOutlet weak var locationZip: UITextField!
+    var id_to_submit=""
     
     let ref = FIRDatabase.database().reference()
     let eventsRef = FIRDatabase.database().reference(withPath: "master-events")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+               // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,19 +39,28 @@ class createEventViewController: UIViewController {
     
     func sendData(lat: Double, withLong:Double)
     {
+        let user = FIRAuth.auth()?.currentUser
+        let uid = user?.uid
+
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MM-YYYY"
+        let strDate = dateFormatter.string(from: datePicker.date)
+        
         let newEventRef = self.ref
             .child("master-events")
             .childByAutoId()
         
         let newEventId = newEventRef.key
+        id_to_submit = newEventId
         let newEventData:[String: AnyObject]  = [
             "event_id": newEventId as AnyObject,
-            "event_name":"Beach Clean Up" as AnyObject,
-            "event_description":"Cleaning up the beach at Half Moon Bay, CA. 20 Volunteers needed for this event. This event will be all day long" as AnyObject,
-            "event_ownerID": "122233444xxxx"as AnyObject ,
-            "event_time": ["event_date":"Date", "event_hour":"16", "event_minute":"40"] as AnyObject,
-            "event_location":["event_Lat":lat, "event_long":withLong,"event_locName":"Half Moon Bay, CA", "Address": ["street_number":"1030","street_name":"Harlan Dr","street_city":"San Jose","street_state":"California", "street_zipcode":"95129"] as AnyObject] as AnyObject,
-            "event_category":"Clean" as AnyObject
+            "event_name":eventName.text as AnyObject,
+            "event_description":eventDescription.text! as AnyObject,
+            "event_ownerID": uid as AnyObject ,
+            "event_time": ["event_date":strDate, "event_hour":"0", "event_minute":"0"] as AnyObject,
+            "event_location":["event_Lat":lat as AnyObject, "event_long":withLong as AnyObject,"event_locName":locationName.text!, "Address": ["street_number":streetNumber.text!,"street_name":streetNumber.text!,"street_city":"San Jose","street_state":locationState.text!, "street_zipcode":locationZip.text!] as AnyObject] as AnyObject,
+            "event_category":eventCategory.text! as AnyObject
         ]
         
         
@@ -58,33 +68,37 @@ class createEventViewController: UIViewController {
         
     }
     
+    
+    
     @IBAction func createEvent(_ sender: Any) {
-        let address:String = "\(self.streetNumber) \(self.locationState) \(self.locationZip)"
-//        self.getLatLngForZip(zipCode: address)
+        let address:String = "\(self.streetNumber.text) \(self.locationState.text) \(self.locationZip.text)"
         print("arrived HERE")
         print(address)
-//        self.addressToCoordinatesConverter(address: address)
+        self.addressToCoordinatesConverter(address: address)
     }
     
     func addressToCoordinatesConverter(address: String) {
         print(address)
-       
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(address) {
+            if let placemarks:[CLPlacemark] = $0 {
+                let latLoc = placemarks[0].location?.coordinate.latitude
+                let longLoc = placemarks[0].location?.coordinate.longitude
+
+                print(latLoc!)
+                print(longLoc!)
+                
+                self.sendData(lat: latLoc!, withLong: longLoc!)
+            } else {
+                print($1)
+            }
+        }
     }
     
-//    func getLatLngForZip(zipCode: String) {
-//        print(zipCode)
-//        let url = NSURL(string: zipCode)
-//        let data = NSData(contentsOf: url! as URL)
-//        let json = try! JSONSerialization.jsonObject(with: data! as Data, options: JSONSerialization.ReadingOptions.allowFragments) as! NSDictionary
-//        if let result = json["results"] as? NSArray {
-//            if let geometry = result[0]["geometry"] as? NSDictionary {
-//                if let location = geometry["location"] as? NSDictionary {
-//                    let latitude = location["lat"] as! Float
-//                    let longitude = location["lng"] as! Float
-//                    print("\n\(latitude), \(longitude)")
-//                }
-//            }
-//        }
-//    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+            let eventView = segue.destination as! EventViewController
+            eventView.toPass = self.id_to_submit
+    }
 
 }
